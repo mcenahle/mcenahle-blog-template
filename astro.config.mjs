e3693af62d5530ecd406a9a0e3bc3b7b4f16b00c
mcenahle <hi@mcenahle.page>
2026-09-02 18:46:13 +0800

    feat: 文章链接添加图标

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,38 @@
-// @ts-check
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
+import rehypeExternalLinks from "rehype-external-links";
 import pagefind from "astro-pagefind";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://blog.mcenahle.dev",
   trailingSlash: "always",
   prefetch: true,
+
   devToolbar: {
     enabled: false,
   },
+
   integrations: [icon(), pagefind()],
+
+  markdown: {
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          target: "_blank",
+          rel: ["noopener", "noreferrer"],
+
+          test(element) {
+            const href = element.properties?.href;
+
+            return typeof href === "string" && /^https?:\/\//.test(href);
+          },
+
+          properties: {
+            className: ["external-link"],
+          },
+        },
+      ],
+    ],
+  },
 });
